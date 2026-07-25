584502e49add6fa182e1e7420bdfbf559c8b382c
Ambareesh Shyam Sundar <ambareesh@gatech.edu>
2026-07-24 17:04:04 -0700

    switch to niri

---
+++ nvim/colors/mini.vim
@@ -0,0 +1,77 @@
+hi clear
+syntax reset
+let g:colors_name = "mini"
+set background=light
+set t_Co=256
+hi Normal guifg=#000000 ctermbg=NONE guibg=#ffffff gui=NONE
+
+hi DiffText guifg=#000000 guibg=NONE
+hi ErrorMsg guifg=#000000 guibg=NONE
+hi WarningMsg guifg=#000000 guibg=NONE
+hi PreProc guifg=#000000 guibg=NONE
+hi Exception guifg=#000000 guibg=NONE
+hi Error guifg=#000000 guibg=NONE
+hi DiffDelete guifg=#000000 guibg=NONE
+hi GitGutterDelete guifg=#000000 guibg=NONE
+hi GitGutterChangeDelete guifg=#000000 guibg=NONE
+hi cssIdentifier guifg=#000000 guibg=NONE
+hi cssImportant guifg=#000000 guibg=NONE
+hi Type guifg=#000000 guibg=NONE
+hi Identifier guifg=#cc0000 guibg=NONE
+hi PMenuSel guifg=#5e11c5 guibg=NONE
+hi Constant guifg=#5e11c5 guibg=NONE
+hi Repeat guifg=#5e11c5 guibg=NONE
+hi DiffAdd guifg=#5e11c5 guibg=NONE
+hi GitGutterAdd guifg=#5e11c5 guibg=NONE
+hi cssIncludeKeyword guifg=#5e11c5 guibg=NONE
+hi Keyword guifg=#5e11c5 guibg=NONE
+hi IncSearch guifg=#ffffff guibg=#d33939
+hi CurSearch guifg=#ffffff guibg=#d33939
+hi Title guifg=#b30909 guibg=NONE
+hi PreCondit guifg=#b30909 guibg=NONE
+hi Debug guifg=#b30909 guibg=NONE
+hi SpecialChar guifg=#b30909 guibg=NONE
+hi Conditional guifg=#b30909 guibg=NONE
+hi Todo guifg=#b30909 guibg=NONE
+hi Special guifg=#b30909 guibg=NONE
+hi Label guifg=#b30909 guibg=NONE
+hi Delimiter guifg=#b30909 guibg=NONE
+hi Number guifg=#0f0fff guibg=NONE
+hi CursorLineNR guifg=#b30909 guibg=NONE
+hi Define guifg=#b30909 guibg=NONE
+hi MoreMsg guifg=#b30909 guibg=NONE
+hi Tag guifg=#b30909 guibg=NONE
+hi String guifg=#b30909 guibg=NONE
+hi MatchParen guifg=#b30909 guibg=NONE
+hi Macro guifg=#b30909 guibg=NONE
+hi DiffChange guifg=#b30909 guibg=NONE
+hi GitGutterChange guifg=#b30909 guibg=NONE
+hi cssColor guifg=#b30909 guibg=NONE
+hi Function guifg=#0055dd guibg=NONE
+hi Directory guifg=#77148d guibg=NONE
+hi markdownLinkText guifg=#77148d guibg=NONE
+hi javaScriptBoolean guifg=#77148d guibg=NONE
+hi Include guifg=#77148d guibg=NONE
+hi Storage guifg=#77148d guibg=NONE
+hi cssClassName guifg=#77148d guibg=NONE
+hi cssClassNameDot guifg=#77148d guibg=NONE
+hi Statement guifg=#ff0055 guibg=NONE gui=NONE
+hi Operator guifg=#000000 guibg=NONE
+hi cssAttr guifg=#000000 guibg=NONE
+
+
+hi Pmenu guifg=#000000 guibg=#eeeeee
+hi SignColumn guibg=#ffffff
+hi Title guifg=#000000
+hi LineNr guifg=#666666 guibg=#ffffff
+hi NonText guifg=#2943c3 guibg=#ffffff
+hi Comment guifg=#006600 gui=NONE
+hi SpecialComment guifg=#2943c3 gui=NONE guibg=NONE
+hi CursorLine guibg=#ffffff
+hi TabLineFill gui=NONE guibg=#ffffff
+hi TabLine guifg=#666666 guibg=#ffffff gui=NONE
+hi StatusLine gui=bold guibg=#000000 guifg=#ffffff
+hi StatusLineNC gui=NONE guibg=#ffffff guifg=#000000
+hi Search guibg=#0923c3 guifg=#ffffff
+hi VertSplit gui=NONE guifg=#ffffff guibg=NONE
+hi Visual gui=NONE guibg=#00ffff
--- nvim/init.lua
+++ nvim/init.lua
@@ -11,21 +11,28 @@ vim.opt.ttimeout = true
 vim.opt.ttimeoutlen = 1
 vim.opt.ttyfast = true
 vim.opt.winborder = "rounded"
--- vim.cmd("colorscheme retrobox")
-vim.cmd("colorscheme vim")
+-- vim.opt.foldmethod = "indent"
+vim.cmd("colorscheme dark2026")
+vim.o.guifont = "Cascadia Code:h14"
+vim.g.neovide_cursor_animation_length = 0
 
+vim.g.mapleader = " "
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, bufopts)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, bufopts)
+
+vim.keymap.set('n', '<leader>bp', function() vim.cmd('bp') end)
+vim.keymap.set('n', '<leader>bn', function() vim.cmd('bn') end)
+vim.keymap.set('n', '<leader>ww', function() print(vim.fn.wordcount().words) end)
 
 vim.cmd[[set completeopt+=menuone,noinsert,popup]]
 vim.keymap.set('i', '<C-space>', function()
@@ -44,6 +51,13 @@ end, { expr = true })
 vim.lsp.config("rust-analyzer", {
     filetypes = { "rust" },
     cmd = {"rust-analyzer"},
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = {
+                allFeatures = true,
+            },
+        },
+    },
     on_attach = function(client, bufnr)
         vim.lsp.completion.enable(true, client.id, bufnr, {
             autotrigger = true,
@@ -68,8 +82,46 @@ vim.lsp.config("svls", {
         })
     end
 })
+vim.lsp.config("clangd", {
+    filetypes = { "c", "cpp" },
+    cmd = {"clangd", "--experimental-modules-support"},
+    on_attach = function(client, bufnr)
+        vim.lsp.completion.enable(true, client.id, bufnr, {
+            autotrigger = true,
+        })
+    end
+})
+vim.lsp.config("uiua", {
+    filetypes = { "uiua" },
+    cmd = {"uiua", "lsp"},
+    root_markers = { 'main.ua', '.fmt.ua', '.git' },
+    on_attach = function(client, bufnr)
+        vim.lsp.completion.enable(true, client.id, bufnr, {
+            autotrigger = true,
+        })
+    end
+})
 vim.lsp.enable({
     "rust-analyzer",
     "python-lsp-server",
     "svls",
+    "clangd",
+    "uiua",
 })
+
+vim.pack.add({
+    "https://github.com/Julian/lean.nvim",
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/nvim-telescope/telescope.nvim",
+})
+
+require("lean").setup{
+    abbreviations = { builtin = true },
+    mappings = true,
+}
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
